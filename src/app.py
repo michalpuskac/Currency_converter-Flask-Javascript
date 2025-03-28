162a8d080f170e77a2b7cad2de875227aaf3b756
Michal Puškáč <133041741+michalpuskac@users.noreply.github.com>
2025-03-28 13:01:52 +0100

    Update-port-in-app

--- src/app.py
+++ src/app.py
@@ -38,5 +38,7 @@ def convert_currency():
         return jsonify({"error": str(e)}), 500
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 #     app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
